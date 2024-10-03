0f7a6e98b3dd936f86507a856fc9b43dffba63a7
James Connelly <jmc8665@psu.edu>
2024-10-03 09:15:21 -0400

    Added peak detection

--- time_of_flight/time_of_flight.ino
+++ time_of_flight/time_of_flight.ino
@@ -2,6 +2,11 @@
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 int value;
+int bufferSize = 5;
+float buffer[5];
+unsigned long lastTime;
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -31,10 +36,43 @@ void loop() {
     value = measure.RangeMilliMeter;
    // Serial.print("Distance (mm): "); 
    Serial.println(value);
+   shiftArrayRight(buffer, bufferSize, value);
+   unsigned long period = FindPeaks(buffer);
+   if(period != 0){
+    //Serial.println(period);
+   }
   } else {
     //Serial.println(" out of range ");
-    Serial.println(-1);
+    //Serial.println(-1);
   }
     
-  delay(100);
+  delay(5);
+}
+
+unsigned long FindPeaks(float buffer[]){
+
+  float mp = buffer[2];
+
+  unsigned long period = 0;
+
+  //Look for peak
+  if(mp > buffer[0] && mp > buffer[1] && mp > buffer[3] && mp > buffer[4]){
+    unsigned long currTime = millis();
+
+    period = currTime - lastTime;
+    lastTime = currTime;
+
+  }
+
+  return period;
+
+}
+
+void shiftArrayRight(float buffer[], int size, float newValue) {
+    
+    for (int i = size - 1; i > 0; i--) {
+        buffer[i] = buffer[i - 1];
+    }
+    // Place the new value in the leftmost spot
+    buffer[0] = newValue;
 }
